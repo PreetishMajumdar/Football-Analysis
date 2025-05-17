5ed0514c396c7bda02f0ce0914de904c756b68b1
anonimity69 <shrayanmandal21@gmail.com>
2025-05-17 14:21:01 +0530

    yolo init

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,3 +1,9 @@
 from ultralytics import YOLO
 import cv2
 
+# Load a pre-trained YOLO model
+model = YOLO("yolo11n.pt")
+
+# Start tracking objects in a video
+# You can also use live video streams or webcam input
+model.track(source="Datasets\\1.mp4")
