03db44910870ad6ec6c3c7b792b799c544e2de9f
anonimity69 <shrayanmandal21@gmail.com>
2025-05-21 19:34:28 +0530

    Changed the necessary details regarding the Classes formed in utility and the object that are formed by it. Fixed minor refactoring issues. Included the pkl stub files for smoother run. Included input_videos folder.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
-    video_frames = VideoUtils.read_video('input_videos/08fd33_4.mp4')
+    video_frames = VideoUtils.read_video('input_videos/4.mp4')
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
